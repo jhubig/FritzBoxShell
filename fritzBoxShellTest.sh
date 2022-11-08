cb4ce2566024088822b6a5dae6ba8c8ffd4c14aa
Denis Imeri <thecrether@gmail.com>
2022-11-08 12:56:35 +0100

    fix one WLAN_5G test case fixing a typo/copy error where WLAN_2G STATE was executed twice, but no WLAN_5G STATE test case existed

--- fritzBoxShellTest.sh
+++ fritzBoxShellTest.sh
@@ -14,7 +14,7 @@ DIRECTORY=$(cd "$dir" && pwd)
 source "$DIRECTORY/fritzBoxShellConfig.sh"
 
 ## declare an array variable
-declare -a services=("WLAN_2G"   "WLAN_2G" "WLAN_5G"    "WLAN_2G" "WLAN"  "LAN"   "DSL"   "WAN"   "LINK"  "IGDWAN" "IGDDSL" "IGDIP" "TAM")
+declare -a services=("WLAN_2G"   "WLAN_2G" "WLAN_5G"    "WLAN_5G" "WLAN"  "LAN"   "DSL"   "WAN"   "LINK"  "IGDWAN" "IGDDSL" "IGDIP" "TAM")
 declare -a actions=("STATISTICS" "STATE"   "STATISTICS" "STATE"   "STATE" "STATE" "STATE" "STATE" "STATE" "STATE"  "STATE"  "STATE" "0 GetInfo")
 declare -a minwords=(3           5         3            5         9       1       1       1       1       1        1        1       5      )
 
